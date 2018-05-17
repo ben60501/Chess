d695813a17a1af5b9231234c025536388390446b
Ben Loan <ben6501@gmail.com>
2018-05-17 10:32:22 -0400

    Started movement for queen

--- Pieces/Queen.java
+++ Pieces/Queen.java
@@ -1,5 +1,7 @@
 package Pieces;
 
+import Chess.Chess;
+
 import java.util.ArrayList;
 
 public class Queen extends GamePiece {
@@ -11,7 +13,65 @@ public class Queen extends GamePiece {
         possibleMoves = new ArrayList<>();
     }
 
+
     public ArrayList<int[]> getPossibleMoves() {
+        possibleMoves = new ArrayList<>();
+
+        //Gets all of the row and column moves
+        ArrayList<int[]> rowMoves = findMovesInRow();
+        ArrayList<int[]> colMoves = findMovesInCol();
+
+        //Adds all of the moves to possible moves
+        possibleMoves.addAll(rowMoves);
+        possibleMoves.addAll(colMoves);
+
         return possibleMoves;
     }
+
+
+    private ArrayList<int[]> findMovesInRow() {
+        //Stores all of the moves in the row
+        ArrayList<int[]> moves = new ArrayList<>();
+
+        //Loops over all of the row and finds the moves that the piece can go
+        for (int col = 0; col < 8; col++) {
+            if (Chess.chessBoard[row][col] == 0) {
+                moves.add(new int[] {row, col});
+            } else if (pieceColor == Color.Black && Chess.chessBoard[row][col] == -1) {
+                moves.add(new int[] {row, col});
+            } else if (pieceColor == Color.Red && Chess.chessBoard[row][col] == 1) {
+                moves.add(new int[] {row, col});
+            }
+        }
+
+        return moves;
+    }
+
+
+    private ArrayList<int[]> findMovesInCol() {
+        //Stores all of the moves in the column
+        ArrayList<int[]> moves = new ArrayList<>();
+
+        //Loops over all of the column and finds the moves that the piece can go
+        for (int col = 0; col < 8; col++) {
+            if (Chess.chessBoard[row][col] == 0) {
+                moves.add(new int[] {row, col});
+            } else if (pieceColor == Color.Black && Chess.chessBoard[row][col] == -1) {
+                moves.add(new int[] {row, col});
+            } else if (pieceColor == Color.Red && Chess.chessBoard[row][col] == 1) {
+                moves.add(new int[] {row, col});
+            }
+        }
+
+        return moves;
+    }
+
+    private ArrayList<int[]> findPosDiagonalMoves() {
+        return new ArrayList<>();
+    }
+
+    private ArrayList<int[]> findNegDiagonalMoves() {
+        return new ArrayList<>();
+    }
+
 }

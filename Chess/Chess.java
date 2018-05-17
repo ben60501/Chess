d1cf855a28b8a6dc0728cf960ef9081883db4853
Ben Loan <ben6501@gmail.com>
2018-05-17 09:56:33 -0400

    Added Board

--- Chess/Chess.java
+++ Chess/Chess.java
@@ -5,6 +5,17 @@ import java.awt.*;
 
 public class Chess extends JPanel {
 
+    public static int[][] chessBoard = {
+            {-1, -1, -1, -1, -1, -1, -1, -1},
+            {-1, -1, -1, -1, -1, -1, -1, -1},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1},
+    };
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
     }
